534f6dcc44fbe0085b8617e39d7acf97abfcb25f
Wade Manuel <epiletaalayo@gmail.com>
2025-02-02 03:37:22 -0300

    He cambiado a postgressql server

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,6 +57,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 import os
+# Import dj-database-url at the beginning of the file.
+import dj_database_url
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -82,10 +85,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database documentation https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/postgres ',
+        conn_max_age=600
+    )
 }
 
 
